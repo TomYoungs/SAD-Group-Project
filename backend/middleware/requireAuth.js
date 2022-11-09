3e9e9f138a3a3e82cbd7a85abb0d703d6a5d3a8c
tomyoungs <tomfyoungs@gmail.com>
2022-11-09 21:19:32 +0000

    Added example files with working login

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/usermodel')
+
+//async?
+const requireAuth = async (req, res, next) => {
+
+    //vertify user auth
+    const { authorization } = req.headers
+
+    if (!authorization) {
+        return res.status(401).json({error: 'auth token required'})
+    }
+
+    //format header to remove junk
+    const token = authorization.split(' ')[1]
+
+    try {
+        const {_id} = jwt.verify(token, process.env.SECRET)
+
+        req.user = await User.findOne({_id}).select('_id')
+        next()
+
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({error: 'req not authorised'})
+    }
+}
+
+module.exports = requireAuth
---
+++ backend/routes/user.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+//controller functions
+const {loginUser, registerUser} = require('../controllers/userController')
+
+//login Route
+router.post('/login', loginUser)
+
+
+//register Route
+router.post('/register', registerUser)
+
+
+module.exports = router
