32e7cc7563f90cac925f1bdf4d78204fbfa5972e
Richard Atkinson <c0002270@my.shu.ac.uk>
2022-11-16 18:43:01 +0000

    Update modulemodel.js

--- backend/models/modulemodel.js
+++ backend/models/modulemodel.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const moduleSchema = new Schema({
-  `name: {
-      type: String,
-      required: true,
-    },
-    Tutors: [String],
-  });
+  name: {
+    type: String,
+    required: true,
+  },
+  Tutors: [String],
+});
 
 
 
