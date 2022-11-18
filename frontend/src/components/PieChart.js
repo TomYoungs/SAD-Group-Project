8fdaa3ad08c864c554bc28abbb3ea4a709c09b89
tomyoungs <tomfyoungs@gmail.com>
2022-11-18 15:37:48 +0000

    reverted PieChart to Richards version

--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -1,39 +1,39 @@
-// import React from 'react'
-// import { useEffect, useState } from 'react';
-// import {Chart as ChartJs, Tooltip, Title, ArcElement, Legend} from "react-chartjs-2";
-// import { Pie } from "react-chartjs-2";
-// ChartJs.register(
-//   Tooltip, Title, ArcElement, Legend
-// );
+import React from 'react'
+import { useEffect, useState } from 'react';
+import {Chart as ChartJs, Tooltip, Title, ArcElement, Legend} from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+ChartJs.register(
+  Tooltip, Title, ArcElement, Legend
+);
 
 
-// //AttendanceForModule is an object with 2 atributes present and Absent
-// //these two variables are the number of present and absent stundent throughout
-// //the modules runtime
-// const PieChart = (AttendanceForModule) => {
-//   console.log(AttendanceForModule)
-//   const data= {
-//   datasets: [{
-//       data: [AttendanceForModule.present, AttendanceForModule.absent],
-//       //ToDo green and red look ugly maybe check with others for better schema?
-//       backgroundColor:[
-//         'Green',
-//         'Red',
-//       ]
-//   },
-// ],
-// //These are purely for the key
-// labels: [
-//     'Present',
-//     'Absent',
-// ],
-// }
+//AttendanceForModule is an object with 2 atributes present and Absent
+//these two variables are the number of present and absent stundent throughout
+//the modules runtime
+const PieChart = (AttendanceForModule) => {
+  console.log(AttendanceForModule)
+  const data= {
+  datasets: [{
+      data: [AttendanceForModule.present, AttendanceForModule.absent],
+      //ToDo green and red look ugly maybe check with others for better schema?
+      backgroundColor:[
+        'Green',
+        'Red',
+      ]
+  },
+],
+//These are purely for the key
+labels: [
+    'Present',
+    'Absent',
+],
+}
 
-//   return (
-//     <div className='pie-chart'>
-//       <Pie data={data}/>
-//     </div>
-//   )
-// }
+  return (
+    <div className='pie-chart'>
+      <Pie data={data}/>
+    </div>
+  )
+}
 
-// export default PieChart
+export default PieChart
