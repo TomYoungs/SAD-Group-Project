e06a568b2f6baf247ad16d108692ec331092f42e
JacobAsuman <75538352+JacobAsuman@users.noreply.github.com>
2022-11-25 18:33:08 +0000

    changed password error output

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -73,7 +73,7 @@ userSchema.statics.login = async function (email, password) {
   const match = await bcrypt.compare(password, user.password)
 
   if (!match){
-    throw Error('password go brrrr')
+    throw Error('password is incorrect')
   }
 
   return user
