5a18333ea6e353596ac5a4c7c2623b5730d663ac
Theo Septiano <b9039310@my.shu.ac.uk>
2022-11-26 15:58:32 +0000

    Users cannot use URL typing to route themselves to places they shouldnt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext'
 import { checkLogin } from './hooks/checkLogin'
 import Home from './pages/Home'
-import Navbar from  './components/Navbar'
+import Navbar from './components/Navbar'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import StudentPage from './pages/StudentPage'
@@ -13,33 +13,37 @@ function App() {
   const { user } = useAuthContext()
 
   return (
-    <div className="App" onload = "checkLogin()">
+    <div className="App">
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <div className='pages'>
           <Routes>
             <Route
               path="/"
               element={user ? <Home /> : <Navigate to="/login" />}
-              />
+            />
             <Route
               path="/register"
               element={!user ? <Register /> : <Navigate to="/" />}
-              />
+            />
             <Route
-              path = "/login"
+              path="/login"
               element={!user ? <Login /> :
-              //if there is a user, performs checks below, role being 0 means student, 4 means admin, rest are a tutor
-              user.role == 0 ? <Navigate to="/studentpage" /> : user.role == 4? <Navigate to="/" /> : <Navigate to="/staffpage" />}
+                //if there is a user, performs checks below, role being 0 means student, 4 means admin, rest are a tutor
+                user.role === 0 ? <Navigate to="/studentpage" /> : user.role === 4 ? <Navigate to="/" /> : <Navigate to="/staffpage" />}
             />
             <Route
-              path="/studentpage" 
-              element={<StudentPage />}
+              path="/studentpage"
+              element={!user ? <Login /> : user.role === 0 ? <Navigate to="/studentpage" /> : <Navigate to="/login" />}
             />
-            <Route 
+            <Route
               path="/staffpage"
-              element={<StaffPage />}
+              element={!user ? <Login /> : user.role <4 && user.role >0 ? <Navigate to="/staffpage" /> : <Navigate to="/login" />}    
             />
+            {/* <Route UNCOMMENT WHEN ADMIN PAGE UP AND RUNNING
+              path="/adminpage"
+              element={!user ? <Login /> : user.role === 4 ? <Navigate to="/adminpage" /> : <Navigate to="/login" />}
+            /> */}
             <Route
               path="*"
               element={<ErrorPage />}
--- frontend/src/hooks/checkLogin.js
+++ frontend/src/hooks/checkLogin.js
@@ -1,17 +1,17 @@
-import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
 
-export const checkLogin = () => {
-        switch (localStorage.getItem(role))
-        {
-            case 0: <Route><Navigate to="/StudentPage" /></Route>
-            break;
-            case 4: <Route><Navigate to="/" /></Route>//Placeholder for AdminPage
-            break;
-            default: <Route><Navigate to="/StaffPage" /></Route>
-            break;
-            case NaN || null:
-                console.log("Error in CheckLogin with local user role");
-                <Route><Navigate to="/login" /></Route>
-            break;
-        }
-}
+// export const checkLogin = (role) => {
+//         switch (role)
+//         {
+//             case 0: <Route><Navigate to="/StudentPage" /></Route>
+//             break;
+//             case 4: <Route><Navigate to="/" /></Route>//Placeholder for AdminPage
+//             break;
+//             default: <Route><Navigate to="/StaffPage" /></Route>
+//             break;
+//             case NaN || null:
+//                 console.log("Error in CheckLogin with local user role");
+//                 <Route><Navigate to="/login" /></Route>
+//             break;
+//         }
+// }
