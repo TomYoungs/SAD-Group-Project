b43f2012010f30aae76dc3f89a50bb52fde582ca
Jess <jessica.s.read@outlook.com>
2022-11-27 00:05:31 +0000

    conflicts resolved
    conflicts resolved from merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { useAuthContext } from './hooks/useAuthContext'
-import { checkLogin } from './hooks/checkLogin'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import StudentPage from './pages/StudentPage'
-import StaffPage from './pages/StaffPage'
-import ErrorPage from './pages/404'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+import { checkLogin } from "./hooks/checkLogin";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import StudentPage from "./pages/StudentPage";
+import StaffPage from "./pages/StaffPage";
+import ErrorPage from "./pages/404";
 
 function App() {
-  const { user } = useAuthContext()
+  const { user } = useAuthContext();
 
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <div className='pages'>
+        <div className="pages">
           <Routes>
             <Route
               path="/"
@@ -28,26 +28,26 @@ function App() {
             />
             <Route
               path="/login"
-              element={!user ? <Login /> :
-                //if there is a user, performs checks below, role being 0 means student, 4 means admin, rest are a tutor
-                user.role === 0 ? <Navigate to="/studentpage" /> : user.role === 4 ? <Navigate to="/" /> : <Navigate to="/staffpage" />}
+              element={
+                !user ? (
+                  <Login />
+                ) : //if there is a user, performs checks below, role being 0 means student, 4 means admin, rest are a tutor
+                user.role === 0 ? (
+                  <Navigate to="/studentpage" />
+                ) : user.role === 4 ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Navigate to="/staffpage" />
+                )
+              }
             />
-            <Route
-              path="/studentpage"
-              element={!user ? <Login /> : user.role === 0 ? <Navigate to="/studentpage" /> : <Navigate to="/login" />}
-            />
-            <Route
-              path="/staffpage"
-              element={!user ? <Login /> : user.role <4 && user.role >0 ? <Navigate to="/staffpage" /> : <Navigate to="/login" />}    
-            />
+            <Route path="/studentpage" element={<StudentPage />} />
+            <Route path="/staffpage" element={<StaffPage />} />
             {/* <Route UNCOMMENT WHEN ADMIN PAGE UP AND RUNNING
               path="/adminpage"
               element={!user ? <Login /> : user.role === 4 ? <Navigate to="/adminpage" /> : <Navigate to="/login" />}
             /> */}
-            <Route
-              path="*"
-              element={<ErrorPage />}
-            />
+            <Route path="*" element={<ErrorPage />} />
           </Routes>
         </div>
       </BrowserRouter>
--- frontend/src/pages/StaffPage.js
+++ frontend/src/pages/StaffPage.js
@@ -1,23 +1,22 @@
 // import { ModulePicker } from "../components/ModulePicker";
 // import { WeekPicker } from "../components/WeekPicker";
-import ModuleDetails from '../components/ModuleDetails'
-import PieChart from '../components/PieChart'
-import AllAttendancePieChart from '../components/AllAttendancePieChart'
+import ModuleDetails from "../components/ModuleDetails";
+import PieChart from "../components/PieChart";
+import AllAttendancePieChart from "../components/AllAttendancePieChart";
 import { useEffect, useState } from "react";
 import { useGenerateCode } from "../hooks/useGenerateCode";
 import Tabs from "../components/Tabs";
 
 const StaffPage = () => {
-  let items = [1,2,3,4,5,6,7,8];
+  let items = [1, 2, 3, 4, 5, 6, 7, 8];
   const [modules, setModule] = useState(null);
   const userid = JSON.parse(localStorage.getItem("user")).id;
   const [moduleID, setModuleID] = useState("");
-  const [codeID, setCodeID ] = useState("")
+  const [codeID, setCodeID] = useState("");
   const [weekID, setWeekID] = useState("");
   const [attendanceWeekStart, setAttendanceWeekStartID] = useState(0);
   const [attendanceWeekEnd, setAttendanceWeekEndID] = useState(8);
   const { generateCode, error, isLoading } = useGenerateCode();
-
 
   const handleGenerate = async (e) => {
     e.preventDefault();
@@ -25,9 +24,9 @@ const StaffPage = () => {
     // console.log(modules[0]._id)
     // console.log(moduleID)
     // console.log(weekID)
-    setCodeID(await generateCode(moduleID, weekID))
-    setModuleID("")
-    setWeekID("")
+    setCodeID(await generateCode(moduleID, weekID));
+    setModuleID("");
+    setWeekID("");
   };
 
   useEffect(() => {
@@ -45,129 +44,147 @@ const StaffPage = () => {
     fetchModules();
   }, []);
 
-
   return (
     <>
-    <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-    </head>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </head>
       <div id="staffTabs">
-      <Tabs>
-        <div label="Today's Code">
-          <div className="default-tab tab1">
-        <form className="module-picker" onSubmit={handleGenerate}>
-          <h2>Generate Code</h2>
-          <div className="module-selector">
+        <Tabs>
+          <div label="Today's Code">
+            <div className="default-tab tab1">
+              <form className="module-picker" onSubmit={handleGenerate}>
+                <h2>Generate Code</h2>
+                <div className="module-selector">
+                  <label>Choose one of your Modules:</label>
+                  <select
+                    onChange={(e) => {
+                      const selectedModule = e.target.value;
+                      setModuleID(selectedModule);
+                    }}
+                  >
+                    <option key="empty" value=""></option>
+                    {modules &&
+                      modules.map((module) => (
+                        <option key={module._id} value={module._id}>
+                          {module.name}
+                        </option>
+                      ))}
+                  </select>
+                </div>
 
-        <label>Choose one of your Modules:</label>
-        <select
-          onChange={(e) => {
-            const selectedModule = e.target.value;
-            setModuleID(selectedModule);
-          }}
-        >
-          <option key="empty" value=""></option>
-          {modules &&
-            modules.map((module) => (
-              <option key={module._id} value={module._id}>
-                {module.name}
-              </option>
-            ))}
-        </select>
-      </div>
-      
+                <div className="week-selector">
+                  <label>Choose a week:</label>
+                  <select
+                    onChange={(e) => {
+                      const selectedWeek = e.target.value;
+                      setWeekID(selectedWeek);
+                    }}
+                  >
+                    <option key="empty" value=""></option>
+                    {items.map((item) => (
+                      <option key={item} value={item}>
+                        Week: {item}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <button
+                  className="default-button"
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  Generate
+                </button>
+                {error && <div className="error">{error}</div>}
+                {codeID && <div className="generated-code">{codeID}</div>}
+              </form>
+            </div>
+          </div>
+          <div label="Modules">
+            <div className="default-tab tab2">
+              <div className="module-info">
+{/* ////////////////////////////////////////////////////////////////////////////////////// */}
 
-      <div className="week-selector">
-      <label>Choose a week:</label>
-      <select
-        onChange={(e) => {
-          const selectedWeek = e.target.value;
-          setWeekID(selectedWeek);
-        }}
-      >
-        <option key="empty" value=""></option>
-        {items.map((item) => (
-          <option key={item} value={item}>
-            Week: {item}
-          </option>
-        ))}
-      </select>
-    </div>
-      <button className="default-button" type="submit" disabled={isLoading}>
-        Generate
-      </button>
-      {error && <div className="error">{error}</div>}
-      {codeID && <div className="generated-code">{codeID}</div>}
-    </form>
-    </div>
-        </div>
-        <div label="Modules">
-          <h2>StaffPage</h2>
-            <div class="grid-container">
-        <div class="grid-item">
-        <p>Starting Week</p>
-        <select
-          onChange={(e) => {
-            const selectedWeek = e.target.value -1;
-            setAttendanceWeekStartID(selectedWeek);
-          }}
-        >
-
-          <option key="empty" value=""></option>
-          {items.map((item) => (
-            <option key={item} value={item}>
-              Week: {item}
-            </option>
-          ))}
-        </select>
-        </div>
-        <div class="grid-item">
-        <p>Ending Week</p>
-        <select
-          onChange={(e) => {
-            const selectedWeek = e.target.value -1;
-            setAttendanceWeekEndID(selectedWeek);
-          }}
-        >
-          <option key="empty" value=""></option>
-          {items.map((item) => (
-            <option key={item} value={item}>
-              Week: {item}
-            </option>
-          ))}
-        </select>
-        </div>
-        </div>
-        <div>
-        {modules &&modules.map((module) => (
-            <ModuleDetails key={module._id} module={module} weekStart = {attendanceWeekStart} weekEnd={attendanceWeekEnd} />
-        ))}
-        </div>
-        <div>
-        {modules && <AllAttendancePieChart key={[attendanceWeekStart,attendanceWeekEnd]} modules={modules} weekStart = {attendanceWeekStart} weekEnd={attendanceWeekEnd} />}
-
-        </div>
-          <div className='default-tab tab2'>
-            <div className='module-info'>
-              <div className='module-list'>
-                {modules &&modules.map((module) => (
-                  <ModuleDetails key={module._id} module={module} />
-                ))}
+              <div class="grid-container">
+              <div class="grid-item">
+                <p>Starting Week</p>
+                <select
+                  onChange={(e) => {
+                    const selectedWeek = e.target.value - 1;
+                    setAttendanceWeekStartID(selectedWeek);
+                  }}
+                >
+                  <option key="empty" value=""></option>
+                  {items.map((item) => (
+                    <option key={item} value={item}>
+                      Week: {item}
+                    </option>
+                  ))}
+                </select>
               </div>
-              <div className='attendance-pie'>
-                {modules && <AllAttendancePieChart modules={modules} />}
+              <div class="grid-item">
+                <p>Ending Week</p>
+                <select
+                  onChange={(e) => {
+                    const selectedWeek = e.target.value - 1;
+                    setAttendanceWeekEndID(selectedWeek);
+                  }}
+                >
+                  <option key="empty" value=""></option>
+                  {items.map((item) => (
+                    <option key={item} value={item}>
+                      Week: {item}
+                    </option>
+                  ))}
+                </select>
               </div>
-              <div className='attendance-pie-mobile'>
-                {modules && <AllAttendancePieChart modules={modules} />}
+            </div>
+{/* ////////////////////////////////////////////////////////////////////////////////////// */}
+
+
+                <div className="module-list">
+                  {modules &&
+                    modules.map((module) => (
+                      <ModuleDetails
+                        key={module._id}
+                        module={module}
+                        weekStart={attendanceWeekStart}
+                        weekEnd={attendanceWeekEnd}
+                      />
+                    ))}
+                </div>
+                <div className="attendance-pie">
+                  {modules && (
+                    <AllAttendancePieChart
+                      key={[attendanceWeekStart, attendanceWeekEnd]}
+                      modules={modules}
+                      weekStart={attendanceWeekStart}
+                      weekEnd={attendanceWeekEnd}
+                    />
+                  )}
+                </div>
+                <div className="attendance-pie-mobile">
+                  {modules && (
+                    <AllAttendancePieChart
+                      key={[attendanceWeekStart, attendanceWeekEnd]}
+                      modules={modules}
+                      weekStart={attendanceWeekStart}
+                      weekEnd={attendanceWeekEnd}
+                    />
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div label="Students">
-          Nothing to see here, this tab is <em>extinct</em>!
-        </div>
-      </Tabs>
-    </div>
+          <div label="Students">
+            Nothing to see here, this tab is <em>extinct</em>!
+          </div>
+        </Tabs>
+      </div>
     </>
   );
 };
